daa48f4b6b54df32aebf4e92d08d31f9f09d0a61
Maynor Romero <maynorariel0@gmail.com>
2025-09-14 18:23:32 -0600

    Commit Inicial

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div align="center">
+      <h1 className="mt-3 text-danger">404 - Not Found!</h1>
+    </div>
+  );
+};
+
+export default NotFound;
